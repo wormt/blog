dee2f11eaad30818d9ff48a95598f3b5a27e8c9f
wormt <209373679+wormt@users.noreply.github.com>
2026-08-16 15:05:01 -0700

    wip: fedora bootc flake

---
+++ nix/flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-caliga = {
+      url = "github:nix-caliga/nix-caliga";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs: {
+    caligaConfigurations.x86_64-linux = {
+      edge = inputs.nix-caliga.lib.makeCaligaConfigurations {
+        pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+        modules = [ ./images/edge ];
+      };
+    };
+  };
+}
---
+++ nix/images/edge/default.nix
@@ -0,0 +1,42 @@
+{ pkgs, ... }:
+
+{
+  config = {
+    caliga.os = "fedora";
+    caliga.core.enable = true;
+    caliga.core.users.enable = true;
+    caliga.core.systemd.enable = true;
+    system.stateVersion = "26.05";
+
+    layeredImage = {
+      name = "ghcr.io/wormt/edge";
+      tag = "latest";
+      fromImage = pkgs.dockerTools.pullImage {
+        imageName = "quay.io/fedora/fedora-bootc";
+        imageDigest = "sha256:295dd6ecda23780e9babf6a889914762ae118c621819d777c879992884d2b681";
+        sha256 = "sha256-wODwf4JGTuJoGjK/QRwZIM97D/abmFsKvQ5wISua7qM=";
+        finalImageTag = "44";
+        arch = "amd64";
+      };
+    };
+
+    # depends on pid 1
+    services.bootc-update = {
+      enable = true;
+      schedule = {
+        onBootSec = "20s";
+        onUnitActiveSec = "20s";
+      };
+    };
+
+    # depends on pid 1
+    users.users.admin = {
+      isNormalUser = true;
+      uid = 1000;
+      description = "wormt";
+      initialPassword = "astronomy-doorknob-amusing-brewery";
+    };
+
+    environment.systemPackages = [ pkgs.nginx ];
+  };
+}
