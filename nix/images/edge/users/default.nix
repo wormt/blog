799735555d7f92cf112c5787e054f312fce2265c
wormt <209373679+wormt@users.noreply.github.com>
2026-08-25 17:21:46 -0700

    fix(vm): ensure /var/home always exists on first boot with a tmpfiles rule

--- nix/images/edge/users/default.nix
+++ nix/images/edge/users/default.nix
@@ -1,4 +1,11 @@
 {
+  # this is necessary if /var/home doesnt exist on first boot.
+  systemd.tmpfiles.settings."00-var-home"."/var/home".d = {
+    mode = "0755";
+    user = "root";
+    group = "root";
+  };
+
   users.users.admin = {
     isNormalUser = true;
     uid = 1000;
