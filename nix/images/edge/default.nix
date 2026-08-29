af5773177907777f01257205b471f3a758170b44
wormt <209373679+wormt@users.noreply.github.com>
2026-08-28 18:44:04 -0700

    chore(vm): bump quay image version

--- nix/images/edge/default.nix
+++ nix/images/edge/default.nix
@@ -11,8 +11,8 @@
       tag = "latest";
       fromImage = pkgs.dockerTools.pullImage {
         imageName = "quay.io/fedora/fedora-bootc";
-        imageDigest = "sha256:b002637dc48abbb1f25f6ab0d8d0572c3b753a691d2917a3fb47a76a10d8b57d"; # registry hash
-        sha256 = "sha256-C01WDuFVrskA+LFoHeBRLMto7AcY88af/qUGXHzz1XA=";                          # nix store hash
+        imageDigest = "sha256:7b7db1d22fe0291fa7e05bc6aeece054b51be5f4857a8ecdb1e69cd368e129d6"; # registry hash
+        sha256 = "sha256-T5ti61b611T3b7/2gD3bfDygIGcP0V2jZYSiKtkwOug=";                          # nix store hash
         finalImageTag = "44";
         arch = "amd64";
       };
