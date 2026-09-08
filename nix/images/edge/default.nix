e305c87f3f2142f187b197bdba22a8e6fa055867
wormt <209373679+wormt@users.noreply.github.com>
2026-09-07 22:02:58 -0700

    wip: maybe this will work

--- nix/images/edge/default.nix
+++ nix/images/edge/default.nix
@@ -24,7 +24,7 @@ in
       config.Labels."org.opencontainers.image.source" = "https://github.com/wormt/blog";
       fromImage = pkgs.dockerTools.pullImage {
         imageName = "quay.io/fedora/fedora-bootc";
-        imageDigest = "sha256:cc0e99fb83e3cf2bd34b073535cfa656dc817dfd29911a1c47546bb013e1c845"; # registry hash
+        imageDigest = "sha256:71e4c657272bba6c9c21c5825b26faec58fdfacf4b62e7643c98d3f50a3a3541"; # registry hash
         sha256 = "sha256-k2ddp1m1FoazicUCa7E8cKmrRlayn6zZPQcjE8qNMjA";                          # nix store hash
         finalImageTag = "44";
         arch = "amd64";
